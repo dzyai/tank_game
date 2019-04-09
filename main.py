85857b370eb6e58059805d50d6b4727740f811fb
dzy <913721288@qq.com>
2019-04-09 20:52:01 +0800

    dzy:窗体显示

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("hello")
-print("hello")
-print("hello")
+"""
+主窗体
+"""
+import sys
+import pygame
+
+from ui.locals import *
+
+if __name__ == '__main__':
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("坦克大战")
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type== pygame.QUIT:
+                sys.exit(0)
+
